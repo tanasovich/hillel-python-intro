b19375428ad13f8596010aa4e8706c87ab67e8c6
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-07-30 21:17:22 +0300

    Added automorphic numbers finder Updated homework README

---
+++ lesson4/find_automorphic_numbers.py
@@ -0,0 +1,6 @@
+n: int = int(input("Write natural number N: "))
+
+for number in range(1, n + 1):
+    squared_number = number**2
+    if str(squared_number).endswith(str(number)):
+        print(f"{number} * {number} = {squared_number}")
