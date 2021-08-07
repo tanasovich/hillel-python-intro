ccb7b9e82942a79dd4c6de5a0870a8ef224d223f
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-07 12:18:31 +0300

    Script populated by description

--- lesson6/find_unique_elements_in_lists.py
+++ lesson6/find_unique_elements_in_lists.py
@@ -3,6 +3,11 @@ import random
 first_list: list = [random.randrange(0, 100) for i in range(15)]
 second_list: list = [random.randrange(0, 100) for i in range(15)]
 
+'''
+Use list comprehension to populate list by zero each time when number exists either only in the first list either only
+in the second list (if-xor condition). Thus, list length equals to the number of unique integers. len function shows
+this parameter (list length).
+'''
 print(len([0 for i in first_list + second_list
            if first_list.count(i) == 1 ^ second_list.count(i) == 1]
           ))
