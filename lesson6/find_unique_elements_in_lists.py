7647c932b7dbd9cda4f054ed7cdc1555c4c33516
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-07 04:37:10 +0300

    Minified script length

--- lesson6/find_unique_elements_in_lists.py
+++ lesson6/find_unique_elements_in_lists.py
@@ -3,10 +3,6 @@ import random
 first_list: list = [random.randrange(0, 100) for i in range(15)]
 second_list: list = [random.randrange(0, 100) for i in range(15)]
 
-unique_numbers_count: int = 0
-
-for i in first_list + second_list:
-    if first_list.count(i) == 1 ^ second_list.count(i) == 1:  # i exists only in the first or only in the second list
-        unique_numbers_count += 1
-
-print(unique_numbers_count)
+print(len([0 for i in first_list + second_list
+           if first_list.count(i) == 1 ^ second_list.count(i) == 1]
+          ))
