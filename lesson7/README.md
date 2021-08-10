b9ee3dc8be5f34182bfdf231c8ef0cec2093a5ed
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-10 23:23:10 +0300

    Added unique numbers counter

--- lesson7/README.md
+++ lesson7/README.md
@@ -1,2 +1,3 @@
 # Lesson 7
 + __count_different_numbers.py__ - count different numbers (sets)
++ __count_unique_numbers_from_lists.py__ - count unique numbers from two lists (sets)
---
+++ lesson7/count_unique_numbers_from_lists.py
@@ -0,0 +1,5 @@
+first_list: list = list(map(int, input("Enter numbers separated by space for the first list: ").split()))
+
+second_list: list = list(map(int, input("Enter numbers separated by space for the second list: ").split()))
+
+print(len(set(first_list) ^ set(second_list)))
