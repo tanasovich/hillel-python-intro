3166e983fd37bc0f98319486d8a74c6000c1aa0a
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-14 12:28:30 +0300

    Implemented sum numbers finder function

--- lesson8/list_and_sum_of_two_ints.py
+++ lesson8/list_and_sum_of_two_ints.py
@@ -1,2 +1,18 @@
 def is_list_has_sum_of_two(numbers: list[int], sum_of_two: int) -> bool:
-    pass
+    if len(numbers) == 0:
+        return False
+
+    for i in range(len(numbers)):
+        for j in (index for index in range(0, len(numbers)) if index != i):
+            if numbers[i] + numbers[j] == sum_of_two:
+                return True
+
+    return False
+
+
+if __name__ == "__main__":
+    print(is_list_has_sum_of_two([1, 2, 3, 4, 5], 7))
+    print(is_list_has_sum_of_two([1, 2, 3, 4, 5], 3))
+    print(is_list_has_sum_of_two([1, 2, 3, 4, 5], 9))
+    print(is_list_has_sum_of_two([1, 2, 3, 4, 5], 10))
+    print(is_list_has_sum_of_two([], 3))
