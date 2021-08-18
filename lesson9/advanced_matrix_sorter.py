562792f3a6964d150d87710e629f280420b77b22
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-18 12:40:12 +0300

    Implemented print matrix function Fixed column summation Added second argument to print matrix function

--- lesson9/advanced_matrix_sorter.py
+++ lesson9/advanced_matrix_sorter.py
@@ -5,8 +5,12 @@ def sort_matrix(matrix: list, column_sums: list) -> None:
     pass
 
 
-def print_matrix_with_sums(matrix: list) -> None:
-    pass
+def print_matrix_with_sums(matrix: list, column_sums: list) -> None:
+    matrix.append(column_sums)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)-1):
+            print(f"{matrix[i][j] : > 4}", end='')
+        print()
 
 
 if __name__ == "__main__":
@@ -21,8 +25,12 @@ if __name__ == "__main__":
     matrix_size: int = int(answer)
 
     matrix: list = [[randint(1, 50) for j in range(matrix_size)] for i in range(matrix_size)]
-    column_sums: list = [sum(column) for column in matrix]
+
+    column_sums: list = [0 for i in range(matrix_size)]
+    for column in range(matrix_size):
+        for row in range(matrix_size):
+            column_sums[column] += matrix[row][column]
 
     sort_matrix(matrix, column_sums)
 
-    print_matrix_with_sums(matrix)
+    print_matrix_with_sums(matrix, column_sums)
