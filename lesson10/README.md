afc9adb97cddf91ef214929f9591cc8ae090c50a
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-22 13:30:28 +0300

    Implemented count_first function Added test cases in the main entry

--- lesson10/README.md
+++ lesson10/README.md
@@ -1,2 +1,3 @@
 # Lesson 10
 + __reverse_number.py__ - module with function, which reverses number
++ __treangle_sequence.py__ - return n numbers from the sequence where each _kth_ elements appears _k_ times
---
+++ lesson10/treangle_sequence.py
@@ -0,0 +1,16 @@
+def count_first(nums, n: int) -> list:
+    def a(k: int) -> int:
+        if k == 1:
+            return 1
+        return a(k - a(k - 1)) + 1
+
+    sequence = list()
+
+    for i in range(1, n + 1):
+        sequence.append(a(i))
+
+    return sequence
+
+
+if __name__ == "__main__":
+    print(count_first(None, 10))
