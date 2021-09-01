224ee6d211568484c04b59e5c07e43bb9af69d69
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-09-01 18:43:40 +0300

    Created negative numbers exception

--- lesson12/README.md
+++ lesson12/README.md
@@ -1,2 +1,3 @@
 # Lesson 12
 + __try_two_values__ - module with function which sums numbers or make concatenation if one of them isn't a number.
++ __only_positive_numbers.py__ - module with custom exception which raises during negative numbers using.
---
+++ lesson12/only_positive_numbers.py
@@ -0,0 +1,21 @@
+class NegativeNumberException(Exception):
+    message = "Trying to use negative numbers! Only positive allowed."
+
+    def __init__(self, message=None):
+        if message:
+            self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+if __name__ == "__main__":
+    try:
+        x = int(input("Please, enter your age: "))
+        if x < 0:
+            raise NegativeNumberException()
+    except NegativeNumberException as e:
+        print(e)
+        exit(-1)
+    else:
+        print(f"You are {x} years old.")
