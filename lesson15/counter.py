7acf886b5677221e762fae87c425ffed3acb3821
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-09-09 09:26:26 +0300

    Built constructor

--- lesson15/counter.py
+++ lesson15/counter.py
@@ -1,6 +1,8 @@
 class Counter:
-    def __init__(self):
-        pass
+    def __init__(self, start: int, stop: int):
+        self.start = start
+        self.current = self.start
+        self.stop = stop
 
     def tick(self) -> int:
         pass
