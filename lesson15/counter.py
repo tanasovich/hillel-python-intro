08fdc80b9144fbfb1561a3132457b1517f7aa7bb
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-09-09 09:40:51 +0300

    Extend with docstrings

--- lesson15/counter.py
+++ lesson15/counter.py
@@ -1,10 +1,28 @@
+"""
+Exposes small and neat Counter class.
+Counter ticking from start to stop value. During each tick Counter returns
+current value.
+"""
+
+
 class Counter:
     def __init__(self, start: int, stop: int):
+        """
+        Creates Counter's object with start-stop values.
+
+        :param start: number where to start ticking
+        :param stop: last possible counter value, impossible to get higher value
+        """
         self.start = start
         self.current = self.start
         self.stop = stop
 
     def tick(self) -> int:
+        """
+        Increments current value until it equals stop attribute.
+
+        :return: current counter's value
+        """
         if self.current < self.stop:
             self.current += 1
             return self.current
@@ -12,6 +30,11 @@ class Counter:
             return self.current
 
     def get_current(self) -> int:
+        """
+        Current getter method.
+
+        :return: current value
+        """
         return self.current
 
 
