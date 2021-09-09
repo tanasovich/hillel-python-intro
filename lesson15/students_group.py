208a7b366487a32616a7b950e57d3e00b0e2e361
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-09-09 10:02:00 +0300

    Implemented Group's printer method

--- lesson15/students_group.py
+++ lesson15/students_group.py
@@ -9,5 +9,14 @@ class Group:
     def __init__(self, students: list[Student]):
         self.students = students
 
-    def print_students_with_scores(self):
-        pass
+    def print_students_with_scores(self, desired_width: int):
+        for student in self.students:
+            grades = ""
+
+            for i in range(len(student.grades)):
+                grades += str(student.grades[i])
+                if i < len(student.grades) - 1:
+                    grades += " "
+
+            print(student.name + grades.rjust(
+                desired_width - len(student.name)))
