67a2f8e16c879f3ce31c519fc59c9a493608b537
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-09-09 09:52:54 +0300

    Implemented Group's constructor Refactor Student

--- lesson15/students_group.py
+++ lesson15/students_group.py
@@ -1,13 +1,13 @@
-class Group:
-    def __init__(self):
-        pass
-
-    def print_students_with_scores(self):
-        pass
-
-
 class Student:
     def __init__(self, name: str, age: int, grades: list[int]):
         self.name = name
         self.age = age
         self.grades = grades
+
+
+class Group:
+    def __init__(self, students: list[Student]):
+        self.students = students
+
+    def print_students_with_scores(self):
+        pass
