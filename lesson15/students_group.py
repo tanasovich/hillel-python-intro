b0a9129c575f8fd7ca781a5ee5c1c4ec6d7ef61e
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-09-09 10:16:21 +0300

    Implemented Group add/remove methods Changed main entry examples

--- lesson15/students_group.py
+++ lesson15/students_group.py
@@ -20,13 +20,29 @@ class Group:
 
             print(student.name + grades.rjust(
                 desired_width - len(student.name)))
+        print()
+
+    def add(self, student: Student) -> None:
+        self.students.append(student)
+        print(f"Added student {student.name}.", end="\n\n")
+
+    def remove(self, student: Student) -> None:
+        self.students.remove(student)
+        print(f"Removed student {student.name}.", end="\n\n")
 
 
 if __name__ == "__main__":
     student1 = Student("John", 18, [3, 3, 3, 3])
     student2 = Student("Nina", 21, [4, 5, 4, 4])
     student3 = Student("Chin", 19, [5, 5, 5, 5])
-    students = [student1, student2, student3]
+    students_list = [student1, student2, student3]
 
-    group = Group(students)
+    group = Group(students_list)
+    group.print_students_with_scores()
+
+    student4 = Student("Benjamin", 22, [4, 4, 4, 4])
+    group.add(student4)
+    group.print_students_with_scores()
+
+    group.remove(student1)
     group.print_students_with_scores()
