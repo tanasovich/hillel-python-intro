59ff6d38612599c09c33bc8ac5d6f98f6751eabf
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-09-09 10:06:48 +0300

    Added main entry point Changed group printer default value

--- lesson15/students_group.py
+++ lesson15/students_group.py
@@ -9,7 +9,7 @@ class Group:
     def __init__(self, students: list[Student]):
         self.students = students
 
-    def print_students_with_scores(self, desired_width: int):
+    def print_students_with_scores(self, desired_width: int = 20):
         for student in self.students:
             grades = ""
 
@@ -20,3 +20,13 @@ class Group:
 
             print(student.name + grades.rjust(
                 desired_width - len(student.name)))
+
+
+if __name__ == "__main__":
+    student1 = Student("John", 18, [3, 3, 3, 3])
+    student2 = Student("Nina", 21, [4, 5, 4, 4])
+    student3 = Student("Chin", 19, [5, 5, 5, 5])
+    students = [student1, student2, student3]
+
+    group = Group(students)
+    group.print_students_with_scores()
