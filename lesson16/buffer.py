78f64ee4f5290d8c98beaded51598224219b52b8
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-09-12 12:11:30 +0300

    Implemented add() method

--- lesson16/buffer.py
+++ lesson16/buffer.py
@@ -4,7 +4,15 @@ class Buffer:
         self.sum_of_five_elements: int = 0
 
     def add(self, *a):
-        pass
+        for number in a:
+            self.elements.append(number)
+
+            if len(self.elements) == 5:
+                for element in self.elements:
+                    self.sum_of_five_elements += element
+                self.elements.clear()
+                print(f"Sum of five elements: {self.sum_of_five_elements}")
+                self.sum_of_five_elements = 0
 
     def get_current_part(self):
         pass
